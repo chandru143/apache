874a2ddabc7f6e50a30aaadaeca73a33c376880f
chandru143 <chandarchandar143@yahoo.co.in>
2020-02-17 21:23:33 +0000

    update with motd info

--- recipes/server.rb
+++ recipes/server.rb
@@ -4,7 +4,11 @@ end
 
 file '/var/www/html/index.html' do
  action :create
- content "This is my second page"
+ content "This is my second page
+ Hostname: #{node['hostname']}
+ Ipaddress: #{node['ipaddress']}
+
+"
  owner "root"
  group "root"
  mode "0755"
