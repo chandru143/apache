966d083624ed35f70b6503974690cfe2a59ead47
chandru143 <chandarchandar143@yahoo.co.in>
2020-02-18 19:20:42 +0000

    replaced index.html from template to cookbook_file

--- recipes/server.rb
+++ recipes/server.rb
@@ -2,15 +2,20 @@ package 'httpd' do
  action :install
 end
 
-template '/var/www/html/index.html' do
- source 'index.html.erb'
- owner "root"
- group "root"
- mode "0755"
- variables(
- :name => 'chandru'
-)
+#template '/var/www/html/index.html' do
+# source 'index.html.erb'
+# owner "root"
+# group "root"
+# mode "0755"
+# variables(
+# :name => 'chandru'
+#)
+#end
+
+cookbook_file '/var/www/html/index.html' do
+ source 'index.html'
 end
+
 
 service 'httpd' do 
  action [:enable, :start]
