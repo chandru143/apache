720e15409edbfdf5bc9e88eb2d605c06e679fda3
chandru143 <chandarchandar143@yahoo.co.in>
2020-02-18 19:04:07 +0000

    upgraded index.html using template

--- recipes/server.rb
+++ recipes/server.rb
@@ -2,16 +2,14 @@ package 'httpd' do
  action :install
 end
 
-file '/var/www/html/index.html' do
- action :create
- content "This is my second page
- Hostname: #{node['hostname']}
- Ipaddress: #{node['ipaddress']}
-
-"
+template '/var/www/html/index.html' do
+ source 'index.html.erb'
  owner "root"
  group "root"
  mode "0755"
+ variables(
+ :name => 'chandru'
+)
 end
 
 service 'httpd' do 
